ffac5d2e60bf953e1519d4d630ab1bb648463e69
Abhiman Kolte <abhimanbhau@icloud.com>
2017-12-11 01:13:24 -0800

    Section 5-41

---
+++ Vars_Consts_DataTypes.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Simple var
+var myAge : Int = 22
+myAge = 24
+
+// Const var
+let myName : String = "Abe"
+let myAgeInTenYears : Int = myAge + 10
+
+let myFullName = "\(myName) Murica"
+
+let myDetails = "\(myFullName), \(myAge)"
+
+let flag = true
+
+let pi = 3.14159265
