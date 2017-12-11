7fc65ecc762923e93676d2ebbda416001047953d
Abhiman Kolte <abhimanbhau@icloud.com>
2017-12-11 00:57:54 -0800

    Section 5-36

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Abhiman Kolte on 12/11/17.
+//  Copyright © 2017 Abhiman Kolte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceViewImage1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+    }
+    
+}
+
